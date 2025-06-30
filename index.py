4c76aa0aad3c5c2a6fdf8b7174d36a4e0f32da44
Iancoski-ops <giancoski321@gmail.com>
2025-06-30 20:28:33 -0300

    elaboração de código para projeto proposto

--- index.py
+++ index.py
@@ -1,2 +1,22 @@
-nome = 'Felipe'
-print(nome)
+nome_heroi = 'Gabriel'
+xp_heroi = 3248
+nivel_heroi = ''
+
+if xp_heroi <= 1000:
+    nivel_heroi = 'Ferro'
+elif xp_heroi > 1000 and xp_heroi <= 2000:
+    nivel_heroi = 'Bronze'
+elif xp_heroi > 2000 and xp_heroi <= 5000:
+    nivel_heroi = 'Prata'
+elif xp_heroi > 5000 and xp_heroi <= 7000:
+    nivel_heroi = 'Ouro'
+elif xp_heroi > 7000 and xp_heroi <= 8000:
+    nivel_heroi = 'Platina'
+elif xp_heroi > 8000 and xp_heroi <= 9000:
+    nivel_heroi = 'Ascendente'
+elif xp_heroi > 9000 and xp_heroi <= 10000:
+    nivel_heroi = 'Imortal'
+elif xp_heroi > 10000:
+    nivel_heroi = 'Radiante'
+
+print(f'O Herói de nome {nome_heroi} está no nível {nivel_heroi}')
